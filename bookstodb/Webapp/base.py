7ded497407d60b2ad1b10df2e347d8b1bf564ec1
armizaguirre <159267121+armizaguirre@users.noreply.github.com>
2024-02-14 12:28:21 +0100

    armando prestiti

--- bookstodb/Webapp/base.py
+++ bookstodb/Webapp/base.py
@@ -131,17 +131,54 @@ def api_loan():
     return jsonify(list_loan)
 
 
-@app.route("/loan")
+@app.route("/loan",  methods=['GET', 'POST'])
 def loan():
     query = """
-    SELECT users.name, GROUP_CONCAT(books.title SEPARATOR ", ") AS title, GROUP_CONCAT(loan.date_of_loan SEPARATOR ", ") AS date_loan
+    SELECT loan.user_id, users.name, GROUP_CONCAT(books.title SEPARATOR ", ") AS title, 
+    GROUP_CONCAT(books.id SEPARATOR ", ") AS book_id, GROUP_CONCAT(loan.date_of_loan SEPARATOR ", ") AS date_loan
     FROM loan JOIN users JOIN books
     ON loan.book_id = books.id AND loan.user_id=users.id
-    GROUP BY users.name;
+    GROUP BY users.name ORDER BY loan.user_id;
     """
     list_loan = execute_query(query)
-    # return jsonify(list_loan)
-    return render_template("prestiti.html", list_loan=list_loan)
+    elimina = []
+    result = []
+    flagaggiunge = False
+    flagelimina = False
+
+    if request.method == 'POST':
+        codice_elimina = [request.form.get('Eliminabook'), request.form.get('Eliminautente')]
+        codice_aggiunge = [request.form.get("Aggiungibook"), request.form.get("Aggiungiutente"), request.form.get("Aggiungidata")]
+        codice_ricerca = request.form.get('Search')
+
+        if codice_ricerca:
+            q_ricerca = f"""
+            SELECT loan.user_id, users.name, GROUP_CONCAT(books.title SEPARATOR ", ") AS title, 
+            GROUP_CONCAT(books.id SEPARATOR ", ") AS book_id, GROUP_CONCAT(loan.date_of_loan SEPARATOR ", ") AS date_loan
+            FROM loan JOIN users JOIN books
+            ON loan.book_id = books.id AND loan.user_id=users.id
+            WHERE loan.user_id LIKE '%{codice_ricerca}%' OR users.name LIKE '%{codice_ricerca}%'
+            GROUP BY users.name ORDER BY loan.user_id;
+            """
+            list_loan = execute_query(q_ricerca)
+
+        if codice_elimina[0] is not None and codice_elimina[1] is not None:
+            flagelimina = True
+            elimina = execute_query(f"SELECT * FROM loan WHERE book_id = {codice_elimina[0]} and user_id = {codice_elimina[1]}")
+            execute_query(f"DELETE FROM loan WHERE book_id = {codice_elimina[0]} and user_id = {codice_elimina[1]}")
+            list_loan = execute_query(query)
+
+
+        if codice_aggiunge[0] is not None and codice_aggiunge[1] is not None and codice_aggiunge[2] is not None :
+            flagaggiunge = True
+            try:
+                execute_query(f"INSERT INTO loan (book_id, user_id, date_of_loan) VALUES ('{codice_aggiunge[0]}', '{codice_aggiunge[1]}', '{codice_aggiunge[2]}')")
+            except:
+                result = ["ERRORE"]
+            list_loan = execute_query(query)
+
+
+    return render_template("prestiti.html", list_loan=list_loan, len=len, elimina=elimina, result=result, flagaggiunge=flagaggiunge, flagelimina=flagelimina)
 
 
 if __name__ == '__main__':
